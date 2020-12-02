f7a2e60cf638d0fbe1c615a0901c948cbec861af
Camilo Pimienta Rosero <camilo040494@gmail.com>
2020-12-02 15:05:12 -0500

    Added query on ChangeRepository bean

--- src/main/java/com/github/camilo/movierental/repository/ChargeRepository.java
+++ src/main/java/com/github/camilo/movierental/repository/ChargeRepository.java
@@ -1,9 +1,13 @@
 package com.github.camilo.movierental.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.repository.CrudRepository;
 
 import com.github.camilo.movierental.model.Charge;
 
 public interface ChargeRepository<T extends Charge> extends CrudRepository<T, Long>{
 
+    public Optional<T> getByTransactionId(String transactionId);
+    
 }
