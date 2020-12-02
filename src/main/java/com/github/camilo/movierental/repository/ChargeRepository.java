889a4aad2da9dd8b7b33278da12b9daed02365d9
Camilo Pimienta Rosero <camilo040494@gmail.com>
2020-12-02 11:20:40 -0500

    Changed JpaRepository by CrudRepository because it won't need all the functionalities

--- src/main/java/com/github/camilo/movierental/repository/ChargeRepository.java
+++ src/main/java/com/github/camilo/movierental/repository/ChargeRepository.java
@@ -1,9 +1,9 @@
 package com.github.camilo.movierental.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 
 import com.github.camilo.movierental.model.Charge;
 
-public interface ChargeRepository<T extends Charge> extends JpaRepository<T, Long>{
+public interface ChargeRepository<T extends Charge> extends CrudRepository<T, Long>{
 
 }
