b5a5faf62a4a0d4e9de70e87620467d6d2c95d08
Camilo Pimienta Rosero <camilo040494@gmail.com>
2020-12-02 10:21:44 -0500

    Charge return type for rental services

--- src/main/java/com/github/camilo/movierental/controller/RentalController.java
+++ src/main/java/com/github/camilo/movierental/controller/RentalController.java
@@ -1,7 +1,5 @@
 package com.github.camilo.movierental.controller;
 
-import java.util.Optional;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,7 +9,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.github.camilo.movierental.messages.ChargeDto;
 import com.github.camilo.movierental.model.OperationEnum;
 import com.github.camilo.movierental.service.RentalService;
 
@@ -24,22 +21,16 @@ public class RentalController{
     
     @PostMapping(value = "/{id}")
     @ResponseBody
-    public ResponseEntity<ChargeDto> rentMovie(@PathVariable long movieId){
-        Optional<ChargeDto> charge = rentService.charge(OperationEnum.RENT, movieId);
-        if (charge.isPresent()) {
-            return new ResponseEntity<ChargeDto>(charge.get(), HttpStatus.OK);
-        }
-        return new ResponseEntity<ChargeDto>(HttpStatus.INTERNAL_SERVER_ERROR);
+    public ResponseEntity<Boolean> rentMovie(@PathVariable long movieId){
+        rentService.charge(OperationEnum.RENT, movieId);
+        return new ResponseEntity<Boolean>(HttpStatus.OK);
     }
     
     @PostMapping(value = "/{id}")
     @ResponseBody
-    public ResponseEntity<ChargeDto> buyMovie(@PathVariable long movieId){
-        Optional<ChargeDto> charge = rentService.charge(OperationEnum.BUY, movieId);
-        if (charge.isPresent()) {
-            return new ResponseEntity<ChargeDto>(charge.get(), HttpStatus.OK);
-        }
-        return new ResponseEntity<ChargeDto>(HttpStatus.INTERNAL_SERVER_ERROR);
+    public ResponseEntity<Boolean> buyMovie(@PathVariable long movieId){
+        rentService.charge(OperationEnum.BUY, movieId);
+        return new ResponseEntity<Boolean>(HttpStatus.OK);
     }
     
 }
--- src/main/java/com/github/camilo/movierental/service/RentalService.java
+++ src/main/java/com/github/camilo/movierental/service/RentalService.java
@@ -1,12 +1,9 @@
 package com.github.camilo.movierental.service;
 
-import java.util.Optional;
-
-import com.github.camilo.movierental.messages.ChargeDto;
 import com.github.camilo.movierental.model.OperationEnum;
 
 public interface RentalService{
     
-    Optional<ChargeDto> charge(OperationEnum operationEnum, long movieId);
+    void charge(OperationEnum operationEnum, long movieId);
     
 }
